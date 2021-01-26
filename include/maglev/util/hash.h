a172095113d23114bd0bff7837c6bb9caa9ac385
lishuangquan <lishq991@gmail.com>
2021-01-26 15:29:38 +0800

    opt MaglevIntHash for 0
    Change-Id: I373986d69ab0ef109edf641e87ba001f0a34501d

--- include/maglev/util/hash.h
+++ include/maglev/util/hash.h
@@ -15,6 +15,7 @@ struct MaglevIntHash {
     x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9ull;
     x = (x ^ (x >> 27)) * 0x94d049bb133111ebull;
     x = x ^ (x >> 31);
+    x += 0x9e3779b97f4a7c16ull;
     return x;
   }
 };
