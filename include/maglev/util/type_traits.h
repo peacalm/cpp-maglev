0dac3398f2f55c869018e241c400e30c41463ffa
lishuangquan <lishq991@gmail.com>
2021-01-27 01:43:27 +0800

    type traits: rm redundant to_void_t
    Change-Id: I9526350f61749e998a6a86df81cefe8ca41b4212

--- include/maglev/util/type_traits.h
+++ include/maglev/util/type_traits.h
@@ -5,18 +5,11 @@
 namespace maglev {
 
 
-template <typename>
-struct to_void { typedef void type; };
-
-template <typename T>
-using to_void_t = typename to_void<T>::type;
-
-
 template <typename NodeT, typename = void>
 struct is_slot_counted : std::false_type {};
 
 template <class NodeT>
-struct is_slot_counted<NodeT, to_void_t<typename NodeT::slot_counted_t>> : std::true_type {};
+struct is_slot_counted<NodeT, typename NodeT::slot_counted_t> : std::true_type {};
 
 template <typename NodeT>
 using is_slot_counted_t = typename is_slot_counted<NodeT>::type;
@@ -27,7 +20,7 @@ template <typename NodeT, typename = void>
 struct is_weighted : std::false_type {};
 
 template <class NodeT>
-struct is_weighted<NodeT, to_void_t<typename NodeT::weighted_t>> : std::true_type {};
+struct is_weighted<NodeT, typename NodeT::weighted_t> : std::true_type {};
 
 template <typename NodeT>
 using is_weighted_t = typename is_weighted<NodeT>::type;
