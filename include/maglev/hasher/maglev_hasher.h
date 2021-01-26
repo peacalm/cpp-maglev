1e0f6c4e9d15506e722e1fc9b6e009a597ab1467
lishuangquan <lishq991@gmail.com>
2021-01-26 22:15:55 +0800

    add hasher
    Change-Id: Iea2d139d80eb4e16ce1606fea2f77fa982e4d2fd

---
+++ include/maglev/hasher/maglev_hasher.h
@@ -0,0 +1,131 @@
+#pragma once
+
+#include <cstring>
+
+#include <array>
+#include <type_traits>
+#include <vector>
+
+#include "maglev/hasher/slot_array.h"
+#include "maglev/node/node_base.h"
+#include "maglev/node_group/node_group_base.h"
+#include "maglev/permutation/permutation_generator.h"
+#include "maglev/util/hash.h"
+
+namespace maglev {
+namespace hasher {
+
+
+template <
+    typename SlotArrayType = SlotArray<int>,
+    typename NodeGroupType = node_group::NodeGroupBase<node::NodeBase<>>,
+    typename PermutationGeneratorType = permutation::PermutationGenerator
+>
+class MaglevHasher {
+public:
+  using slot_array_t = SlotArrayType;
+  using slot_int_t = typename slot_array_t::int_t;
+  using node_group_t = NodeGroupType;
+  using perm_gen_t = PermutationGeneratorType;
+  using perm_gen_array_t = std::vector<perm_gen_t>;
+  using node_t = typename node_group_t::node_t;
+  using node_ptr_t = typename node_group_t::node_ptr_t;
+  using node_group_item_t = typename node_group_t::item_t;
+
+public:
+  struct PickRet {
+    node_group_item_t node;
+    size_t node_idx = 0;
+  };
+  using pick_ret_t = PickRet;
+
+public:
+
+  MaglevHasher() {}
+
+  template<typename NodeGroupT, typename ...Args>
+  MaglevHasher(NodeGroupT&& ng, Args&&...args) :
+      node_group_(std::forward<NodeGroupT>(ng)),
+      slot_array_(std::forward<Args>(args)...) {}
+
+  slot_array_t& slot_array() { return slot_array_; }
+  const slot_array_t& const_slot_array() const { return slot_array_; }
+  size_t slot_size() const { return slot_array_.size(); }
+
+  node_group_t& node_group() { return node_group_; }
+  const node_group_t& const_node_group() const { return node_group_; }
+  size_t node_size() const { return node_group_.size(); }
+
+  pick_ret_t pick(size_t hashed_key) const {
+    pick_ret_t ret;
+    ret.node_idx = slot_array_[hashed_key % slot_size()];
+    ret.node = node_group_[ret.node_idx];
+    return ret;
+  }
+
+  template<typename KeyType, typename HashType = util::def_hash_t<KeyType>>
+  pick_ret_t pick_with_auto_hash(const KeyType& key) {
+    static auto h = HashType{};
+    return pick(h(key));
+  }
+
+  void build() {
+    init_slot_array();
+    init_node_group();
+    auto p = make_perm_gen_array();
+    const auto n = node_size();
+    for (size_t node_idx = 0, slot_distributed_cnt = 0; slot_distributed_cnt < slot_size(); ) {
+      select_once(p[node_idx], node_idx, slot_distributed_cnt);
+      if (++node_idx >= n) node_idx = 0;
+    }
+  }
+
+protected:
+  void init_node_group() { node_group_.ready_go(); }
+
+  constexpr slot_int_t slot_initial_value() const { return slot_int_t(-1); }
+
+  void init_slot_array() {
+    memset((void*)slot_array_.data(), -1, sizeof(slot_int_t) * slot_array_.size());
+  }
+
+  bool is_slot_distributed(size_t idx) const {
+    return slot_array_[idx] != slot_initial_value();
+  }
+
+  void distribut_slot(size_t slot_idx, size_t node_idx) {
+    slot_array_[slot_idx] = (slot_int_t)node_idx;
+  }
+
+  void select_once(perm_gen_t& perm_gen, size_t& node_idx, size_t& slot_distributed_cnt) {
+    while (true) {
+      auto t = perm_gen.gen_one_num();
+      if (!is_slot_distributed(t)) {
+        distribut_slot(t, node_idx);
+        ++slot_distributed_cnt;
+        break;
+      }
+    }
+  }
+
+  perm_gen_t make_a_perm_gen(const node_ptr_t& i) const {
+    return perm_gen_t(slot_size(), i->id_hash());
+  }
+
+  perm_gen_array_t make_perm_gen_array() const {
+    perm_gen_array_t p;
+    p.reserve(node_size());
+    for (const auto& i: node_group_) {
+      p.push_back(make_a_perm_gen(i));
+    }
+    return p;
+  }
+
+private:
+  slot_array_t slot_array_;
+  node_group_t node_group_;
+};
+
+
+}  // namespace hasher
+}  // namespace maglev
---
+++ include/maglev/hasher/slot_array.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <array>
+#include <type_traits>
+#include <vector>
+
+#include "maglev/util/prime.h"
+
+namespace maglev {
+namespace hasher {
+
+
+template <typename IntType = int, size_t SlotNum = 65537>
+class SlotArray: public std::array<IntType, SlotNum> {
+  using base_t = std::array<IntType, SlotNum>;
+
+public:
+  using int_t = typename std::enable_if<std::is_integral<IntType>::value && util::is_prime(SlotNum), IntType>::type;
+};
+
+
+template <typename IntType = int>
+class SlotVector: public std::vector<IntType> {
+  using base_t = std::vector<IntType>;
+  using size_type = typename base_t::size_type;
+  using const_reference = typename base_t::const_reference;
+
+public:
+  using int_t = typename std::enable_if<std::is_integral<IntType>::value, IntType>::type;
+
+public:
+  SlotVector() {}
+  SlotVector(size_type n) : base_t(n) { assert(util::is_prime(n)); }
+
+  void resize(size_type n) { assert(util::is_prime(n)); base_t::resize(n); }
+  void resize(size_type n, const_reference v) { assert(util::is_prime(n)); base_t::resize(n, v); }
+
+};
+
+
+}  // namespace hasher
+}  // namespace maglev
