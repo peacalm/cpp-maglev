dd6dfc294379e6abeb2e170f66176ceb8fd8606c
lishuangquan <lishq991@gmail.com>
2021-02-09 17:46:25 +0800

    ull to ULL
    Change-Id: I7fe1ac31f890a687b4d9859cc47013875f11561c

--- include/maglev/util/hash.h
+++ include/maglev/util/hash.h
@@ -12,10 +12,10 @@ struct MaglevIntHash {
 
   size_t operator()(type n) const {
     size_t x = static_cast<size_t>(n);
-    x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9ull;
-    x = (x ^ (x >> 27)) * 0x94d049bb133111ebull;
+    x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9ULL;
+    x = (x ^ (x >> 27)) * 0x94d049bb133111ebULL;
     x = x ^ (x >> 31);
-    x += 0x9e3779b97f4a7c16ull;
+    x += 0x9e3779b97f4a7c16ULL;
     return x;
   }
 };
